a5cebfdf0d958bc69ca428c693325ce46c2e8289
RostCode78 <zeroxcode78@gmail.com>
2023-08-01 02:03:08 -0600

    Feat: Validaciones basicas

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -13,7 +13,49 @@ export const CalculadoraScreen = () => {
     };
 
     const ArmarNumero = ( numeroTexto: string ) => {
-        setNumero( numero + numeroTexto );
+
+        // No aceptar doble punto
+        if ( numero.includes('.') && numeroTexto === '.' ) { return; }
+
+        if ( numero.startsWith('0') || numero.startsWith('-0') ) {
+
+            // Punto decimal
+            if ( numeroTexto === '.' ) {
+                setNumero( numero + numeroTexto );
+
+                // Evaluar si es otro cero y hay un punto
+            } else if ( numeroTexto === '0' && numero.includes('.') ) {
+
+                setNumero( numero + numeroTexto );
+
+                // Evaluar si es diferente a cero y no tiene un punto
+            } else if ( numeroTexto !== '0' && !numero.includes('.') ) {
+
+                setNumero( numeroTexto );
+
+                // Evitar 0000.0
+            } else if ( numeroTexto === '0' && !numero.includes('.') ) {
+
+                setNumero( numero );
+
+            } else {
+
+                setNumero( numero + numeroTexto );
+
+            }
+
+        } else {
+            setNumero( numero + numeroTexto );
+        }
+
+    };
+
+    const PositivoNegativo = () => {
+        if ( numero.includes('-') ) {
+            setNumero( numero.replace('-', '') );
+        } else {
+            setNumero( '-' + numero );
+        }
     };
 
     return (
@@ -30,7 +72,7 @@ export const CalculadoraScreen = () => {
             {/* Fila de botones */}
             <View style={ styles.fila }>
                 <BotonCalc texto="C" color="#9B9B9B" accion={ Limpiar } />
-                <BotonCalc texto="+/-" color="#9B9B9B" accion={ Limpiar } />
+                <BotonCalc texto="+/-" color="#9B9B9B" accion={ PositivoNegativo } />
                 <BotonCalc texto="del" color="#9B9B9B" accion={ Limpiar } />
                 <BotonCalc texto="/" color="#FF9427" accion={ Limpiar } />
             </View>
